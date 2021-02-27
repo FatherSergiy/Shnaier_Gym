0ef7c794f1d61fc9b097262717f60de5bfac9150
pc01 <>
2021-02-27 15:08:04 +0700

    Final

--- app/src/main/java/com/example/shnayder_gym/SignInActivity.java
+++ app/src/main/java/com/example/shnayder_gym/SignInActivity.java
@@ -6,21 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
-
+TextView name;
+TextView password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        name=(TextView)findViewById(R.id.editTextTextPersonName);
+        password=(TextView)findViewById(R.id.editTextTextPersonName2);
     }
     public void onClick(View v){
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.button4:
-                startActivity(new Intent(SignInActivity.this, MainActivity.class));
-                break;
+                if(name.getText().toString().equals("")||password.getText().toString().equals(""))
+            {
+                Toast.makeText(getApplicationContext(), "Incorrect Name or Password", Toast.LENGTH_SHORT).show();
+            }else{
+                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            }
+            break;
             case R.id.textView4:
-                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+            startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+            break;
         }
     }
 }
--- app/src/main/java/com/example/shnayder_gym/StepActivity5.java
+++ app/src/main/java/com/example/shnayder_gym/StepActivity5.java
@@ -1,14 +1,27 @@
 package com.example.shnayder_gym;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StepActivity5 extends AppCompatActivity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step5);
+        btn=(Button)findViewById(R.id.button24);
+    }
+    public void onClick(View v){
+        Resources res=getResources();
+        Drawable yellow= ResourcesCompat.getDrawable(res, R.drawable.button_yellow, null);
+        btn.setBackground(yellow);
+        startActivity(new Intent(this, SignInActivity.class));
     }
 }
